0da807b72052c0aa4a6f51fbe6e04be036f13235
Darja Strahlberg <darja.strahlberg@gmail.com>
2022-06-21 18:10:48 +0200

    Initial commit

---
+++ lib/my-lambda.ts
@@ -0,0 +1,26 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+
+
+
+export class CdkStarterStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // 👇 lambda function definition
+        const lambdaFunction = new lambda.Function(this, 'lambda-function', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            memorySize: 1024,
+            timeout: cdk.Duration.seconds(5),
+            handler: 'index.main',
+            code: lambda.Code.fromAsset(path.join(__dirname, '/../src/index')),
+            environment: {
+                REGION: cdk.Stack.of(this).region,
+                AVAILABILITY_ZONES: JSON.stringify(
+                    cdk.Stack.of(this).availabilityZones,
+                ),
+            },
+        });
+    }
+}
