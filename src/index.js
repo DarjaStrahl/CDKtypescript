71f5fc588de79fd7a5d063552a26530a1f4d9018
Darja Strahlberg <darja.strahlberg@gmail.com>
2022-06-27 17:46:26 +0200

    Update: main function

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const configs = [{
     },
     ]
 
-;exports.main = async (event) => {
+exports.main = async (event) => {
     // Display user's balance
     let eurBalance = (await kraken.api('Balance')).result.ZEUR
     console.log( 'eurBalance', eurBalance );
@@ -60,4 +60,4 @@ const configs = [{
 
     }
 
-}();
+}
